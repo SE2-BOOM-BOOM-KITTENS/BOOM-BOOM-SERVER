b678ed0872c64264d4e7f4a7e4239a27f9259abd
Yü Li Chang Lin <40342792+Yuelichang@users.noreply.github.com>
2025-03-19 10:57:50 +0100

    added sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "com.aau.se2"
@@ -37,4 +38,12 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "SE2-BOOM-BOOM-KITTENS_BOOM-BOOM-KITTENS")
+		property("sonar.organization", "se2-boom-boom-kittens")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
