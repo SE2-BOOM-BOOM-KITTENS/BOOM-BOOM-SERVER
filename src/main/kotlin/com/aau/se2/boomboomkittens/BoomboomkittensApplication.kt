d56e988250bf169153b40f8583f2b064035215cf
Yü Li Chang Lin <40342792+Yuelichang@users.noreply.github.com>
2025-03-19 10:41:52 +0100

    initial commit

---
+++ src/main/kotlin/com/aau/se2/boomboomkittens/BoomboomkittensApplication.kt
@@ -0,0 +1,11 @@
+package com.aau.se2.boomboomkittens
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoomboomkittensApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoomboomkittensApplication>(*args)
+}
---
+++ src/test/kotlin/com/aau/se2/boomboomkittens/BoomboomkittensApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aau.se2.boomboomkittens
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoomboomkittensApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
