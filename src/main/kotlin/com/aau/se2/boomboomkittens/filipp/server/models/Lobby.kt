626fbc96da47f4399c6b8489bba4ca6dff5c4bbd
filipped <82341252+FilippEder@users.noreply.github.com>
2025-04-07 17:31:43 +0200

    Add creator and maxPlayers params in Lobby.kt

--- src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/models/Lobby.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/models/Lobby.kt
@@ -2,4 +2,7 @@ package com.aau.se2.boomboomkittens.filipp.server.models
 
 import java.util.UUID
 
-data class Lobby(val id:UUID = UUID.randomUUID(), val players:MutableList<Player>)
+data class Lobby(val id:UUID = UUID.randomUUID(),
+                 val creator:Player,
+                 val players:MutableList<Player>,
+                 val maxPlayers:Int)
