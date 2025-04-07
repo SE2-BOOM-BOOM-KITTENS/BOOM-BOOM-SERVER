8f9aa194e3115fdb3e745cf85ea383da60dea738
filipped <82341252+FilippEder@users.noreply.github.com>
2025-04-07 18:53:43 +0200

    Create LobbyWebSocketController

---
+++ src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/controllers/webSocket/LobbyWebSocketController.kt
@@ -0,0 +1,46 @@
+package com.aau.se2.boomboomkittens.filipp.server.controllers.webSocket
+
+import com.aau.se2.boomboomkittens.filipp.server.services.LobbyService
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/lobby")
+class LobbyWebSocketController(
+    private val lobbyService: LobbyService,
+    private val messagingTemplate: SimpMessagingTemplate
+) {
+
+    data class ReadyMessage(val lobbyId:String, val playerId: String)
+    data class LobbyStatusUpdate(
+        val lobbyId: String,
+        val playersReady: Int,
+        val allReady: Boolean
+    )
+
+    @MessageMapping("lobbies")
+    fun requestLobbies(){
+        val lobbies = lobbyService.getLobbies()
+        messagingTemplate.convertAndSend("/topic/lobbies",lobbies)
+    }
+
+    @MessageMapping("/ready")
+    @SendTo("/topic/lobby/status/{lobbyId}")
+    fun handlePlayerReady(message:ReadyMessage): LobbyStatusUpdate{
+        println("Player ${message.playerId} is ready in lobby ${message.lobbyId}")
+
+        return LobbyStatusUpdate(
+            lobbyId = message.lobbyId,
+            playersReady = 3,
+            allReady = false
+        )
+    }
+
+    fun broadcastLobbyUpdate(){
+        val lobbies = lobbyService.getLobbies()
+        messagingTemplate.convertAndSend("/topic/lobbies", lobbies)
+    }
+}
