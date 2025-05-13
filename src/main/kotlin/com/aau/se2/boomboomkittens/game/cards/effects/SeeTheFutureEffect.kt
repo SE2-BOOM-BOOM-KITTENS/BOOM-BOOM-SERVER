7d68e4cfde0895f148a37312b9caa3275ab1d60d
elsarny <elsarny@edu.aau.at>
2025-05-13 08:46:56 +0200

    SeeTheFutureEffect Klasse wurde implementiert

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/SeeTheFutureEffect.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/SeeTheFutureEffect.kt
@@ -4,13 +4,9 @@ import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.GameLo
 import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffect
 import com.aau.se2.boomboomkittens.game.player.Player
 
-/**
- * Commented Out for Rework
- */
-
-//class SeeTheFutureEffect : CardEffect {
-//    override fun apply(player: Player, gameLogic: GameLogic) {
-//        val topCards = gameLogic.cardLogic.peekTopCards(3) // Nimm die obersten 3 Karten
-//        println("${player.name} looked at the top 3 cards: $topCards")
-//    }
-//}
+class SeeTheFutureEffect : CardEffect {
+    override fun apply(player: Player, gameLogic: GameLogic) {
+        val topCards = gameLogic.peekTopCards(3) // Nimm die obersten 3 Karten
+        println("${player.name} looked at the top 3 cards: $topCards")
+    }
+}
