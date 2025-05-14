65ce473f2b3dd313aaa42699a01c9591a1a6815d
elsarny <elsarny@edu.aau.at>
2025-05-14 21:06:04 +0200

    GameLogicTest-Klasse angepasst

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/NopeEffect.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/NopeEffect.kt
@@ -4,9 +4,9 @@ import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.GameLo
 import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffect
 import com.aau.se2.boomboomkittens.game.player.Player
 
-class NopeEffect : CardEffect {
+/*class NopeEffect : CardEffect {
     override fun apply (player: Player, gameLogic: GameLogic){
         gameLogic.nextTurn()
         println ("Nope got played. Your turn was skipped.")
     }
-}
+}*/
