9681321ea3471f81c8609b6e4f2a47b3c5fc7733
elsarny <elsarny@edu.aau.at>
2025-05-14 19:33:56 +0200

    GameLogic angepasst

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/logic/GameLogic.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/logic/GameLogic.kt
@@ -1,6 +1,6 @@
-package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic
-
 import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffectRegistry
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.CardLogic
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.PlayerLogic
 import com.aau.se2.boomboomkittens.game.cards.Card
 import com.aau.se2.boomboomkittens.game.cards.CardPile
 import com.aau.se2.boomboomkittens.game.cards.CardType
@@ -11,71 +11,74 @@ import java.util.UUID
 open class GameLogic(
     var lobbyId: UUID,
     val players: MutableList<Player> = mutableListOf(),
-){
-  private val _playerLogic: PlayerLogic = PlayerLogic()
+) {
+    private val _playerLogic: PlayerLogic = PlayerLogic()
     private val _cardLogic: CardLogic = CardLogic(players.size)
     private val _discardPile: CardPile = CardPile()
     private val _cardRegistry = CardEffectRegistry
 
-    val playerLogic: PlayerLogic
-        get() = _playerLogic
-    val cardLogic: CardLogic
-        get() = _cardLogic
-    val discardPile: CardPile
-        get() = _discardPile
-    val cardRegistry: CardEffectRegistry
-        get() = _cardRegistry
-    val drawPile: LinkedList<Card> = LinkedList()  
-  init {
-        for(player in players){
+    val playerLogic: PlayerLogic get() = _playerLogic
+    val cardLogic: CardLogic get() = _cardLogic
+    val discardPile: CardPile get() = _discardPile
+    val cardRegistry: CardEffectRegistry get() = _cardRegistry
+
+    val drawPile: LinkedList<Card> = LinkedList()
+
+    init {
+        for (player in players) {
             _playerLogic.addPlayerByID(player)
             _cardLogic.addPlayer(player)
         }
     }
 
-    fun removePlayer(playerId: UUID){
+    fun removePlayer(playerId: UUID) {
         _playerLogic.removePlayerByID(playerId)
     }
 
     fun getWinner(): Player? {
-        if(_playerLogic.getPlayerCount() == 1){
-            val winner = _playerLogic.getCurrentPlayer()
-            return winner
-        }
-        return null
+        return if (_playerLogic.getPlayerCount() == 1) {
+            _playerLogic.getCurrentPlayer()
+        } else null
     }
 
-    fun nextTurn(){
+    fun nextTurn() {
         _playerLogic.moveToNextPlayer()
     }
 
-    fun addPlayer(playerId: UUID, playerName:String){
+    fun addPlayer(playerId: UUID, playerName: String) {
         val newPlayer = Player(playerId, playerName)
         _playerLogic.addPlayerByID(newPlayer)
     }
 
-    fun playCard(playerId: UUID, cardType: CardType){
+    fun playCard(playerId: UUID, cardType: CardType) {
         val player = _playerLogic.getPlayerByID(playerId)
-        if(player!!.playerHand.containsCardType(cardType)){
-            val card = _cardRegistry.getEffect(cardType)
-            card.apply(player,this)
-        } else{
+            ?: throw IllegalArgumentException("Player not found: $playerId")
+
+        if (player.playerHand.containsCardType(cardType)) {
+            val effect = _cardRegistry.getEffect(cardType)
+            effect.apply(player, this)
+        } else {
             throw IllegalStateException("Player doesn't have card type $cardType")
         }
-
     }
+
     open fun peekTopCards(count: Int): List<Card> {
         return drawPile.take(count)
     }
+
     open fun allowPlayerToRearrangeTopCards(player: Player, newOrder: List<Card>) {
         if (newOrder.size > drawPile.size) {
             throw IllegalArgumentException("New order has more cards than the draw pile.")
         }
-        repeat(newOrder.size) { drawPile.removeFirst() }
+
+        repeat(newOrder.size) {
+            drawPile.removeFirst()
+        }
 
         for (i in newOrder.size - 1 downTo 0) {
             drawPile.add(newOrder[i])
         }
+
         println("${player.name} rearranged the top ${newOrder.size} cards.")
     }
 }
