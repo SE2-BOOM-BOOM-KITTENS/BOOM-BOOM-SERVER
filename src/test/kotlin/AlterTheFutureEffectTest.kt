2027e6e265b07ca3ef9fba32862cd5b194f44f57
elsarny <elsarny@edu.aau.at>
2025-05-14 14:10:46 +0200

    Alter the Future-Testklasse angepasst

--- src/test/kotlin/AlterTheFutureEffectTest.kt
+++ src/test/kotlin/AlterTheFutureEffectTest.kt
@@ -33,7 +33,7 @@ class AlterTheFutureEffectTest {
         effect.apply(player, gameLogic)
 
         assertNotNull(rearrangedCards)
-        assertEquals("Alter_the_Future", rearrangedCards!![0].name)
+        assertEquals("ALTER_THE_FUTURE", rearrangedCards!![0].name)
         assertEquals("Skip", rearrangedCards!![1].name)
         assertEquals("Shuffle", rearrangedCards!![2].name)
     }
