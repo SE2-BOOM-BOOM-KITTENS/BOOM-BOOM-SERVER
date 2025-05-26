100594d90c2b01f7d02a51e1670a7e3cd8f2c95d
jufreiburgha <147612527+jufreiburgha@users.noreply.github.com>
2025-05-26 19:32:51 +0200

    CatComboMessage gelöscht

--- src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/networkPacket/messages/ComboType.kt
+++
@@ -1,7 +0,0 @@
-package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.networkPacket.messages
-
-enum class ComboType {
-    RANDOM_STEAL,
-    SPECIFIC_REQUEST,
-    DISCARD_RETRIEVE
-}
