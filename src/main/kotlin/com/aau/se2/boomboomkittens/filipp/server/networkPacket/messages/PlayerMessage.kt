a302f4006d40b62ab6ebbdc2f46b6fa006f2cde5
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-13 21:17:16 +0200

    Set PlayerMessage to include lobbyId

--- src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/networkPacket/messages/PlayerMessage.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/filipp/server/networkPacket/messages/PlayerMessage.kt
@@ -1,8 +1,11 @@
 package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.networkPacket.messages
 
+import java.util.UUID
+
 data class PlayerMessage(
     val playerName: String?,
     val action: String?,
     val payload: Any?,
-    val targetId: String? = null
+    val targetId: String? = null,
+    val lobbyId: UUID = UUID.randomUUID(),
     )
