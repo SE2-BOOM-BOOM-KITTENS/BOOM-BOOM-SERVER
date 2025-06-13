9e3391fa855409fadb802835901e517bfeec01b6
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-13 21:16:53 +0200

    Fix GameLogicServiceTest.kt

--- src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/services/GameLogicServiceTest.kt
+++ src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/services/GameLogicServiceTest.kt
@@ -4,28 +4,35 @@ import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.ser
 import com.aau.se2.boomboomkittens.game.cards.Card
 import com.aau.se2.boomboomkittens.game.cards.CardType
 import com.aau.se2.boomboomkittens.game.player.Player
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import java.util.UUID
 
 class GameLogicServiceTest {
 
     private lateinit var service: GameLogicService
     private val messagingTemplate: SimpMessagingTemplate = mock()
+    private val jacksonObjectMapper: ObjectMapper = mock()
 
     private lateinit var player: Player
     private lateinit var target: Player
 
+    //QUICK FIX; SHOULD BE REIMPLEMENTED LATER
+    private var lobbyId: UUID? = null
+
     @BeforeEach
     fun setup() {
-        service = GameLogicService(messagingTemplate)
+        service = GameLogicService(messagingTemplate,jacksonObjectMapper)
+        lobbyId = service.lobbyId
         player = Player(name = "Tester")
         target = Player(name = "Opfer")
-        service.joinGame(player.playerId, player.name)
-        service.joinGame(target.playerId, target.name)
+        service.joinGame(lobbyId!!,player.playerId, player.name)
+        service.joinGame(lobbyId!!,target.playerId, target.name)
     }
 
     @Test
@@ -37,7 +44,7 @@ class GameLogicServiceTest {
 
         val cards = listOf(Card(CardType.CAT_TACO), Card(CardType.CAT_TACO))
 
-        service.playCatCombo(player.playerId, cards, target.playerId)
+        service.playCatCombo(lobbyId!!,player.playerId, cards, target.playerId)
 
         //assertEquals(1, player.playerHand.getCardAmount())
         //assertEquals(0, target.playerHand.getCardAmount())
@@ -46,11 +53,11 @@ class GameLogicServiceTest {
     @Test
     fun `chooseFromDiscard should move card to player hand`() {
         val discardCard = Card(CardType.SHUFFLE)
-        service.getGameLogic().discardPile.add(discardCard)
+        service.getGame(lobbyId!!).discardPile.add(discardCard)
 
-        service.chooseFromDiscard(player.playerId, CardType.SHUFFLE)
+        service.chooseFromDiscard(lobbyId!!,player.playerId, CardType.SHUFFLE)
 
-        val hand = service.getGameLogic().getPlayerById(player.playerId)?.playerHand
+        val hand = service.getGame(lobbyId!!).getPlayerById(player.playerId)?.playerHand
         assertTrue(hand!!.containsCardType(CardType.SHUFFLE))
     }
 }
