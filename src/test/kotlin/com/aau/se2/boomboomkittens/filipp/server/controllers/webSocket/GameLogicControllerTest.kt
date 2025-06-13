11ac1e2da588f59f0b8ba21dfa5df82879462e69
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-13 22:18:01 +0200

    Update GameLogicControllerTest.kt

--- src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/controllers/webSocket/GameLogicControllerTest.kt
+++ src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/controllers/webSocket/GameLogicControllerTest.kt
@@ -1,7 +1,20 @@
 package com.aau.se2.boomboomkittens.filipp.server.controllers.webSocket
 
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.controllers.webSocket.GameLogicController
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.networkPacket.messages.PlayerMessage
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.services.GameLogicService
+import com.aau.se2.boomboomkittens.filipp.server.networkPacket.CardNetworkPacket
+import com.aau.se2.boomboomkittens.game.cards.CardType
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argThat
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.isNull
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
@@ -11,6 +24,8 @@ import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.web.socket.client.standard.StandardWebSocketClient
 import org.springframework.web.socket.messaging.WebSocketStompClient
+import java.security.Principal
+import java.util.UUID
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
 
@@ -18,6 +33,76 @@ import java.util.concurrent.TimeUnit
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GameLogicControllerTest {
+
+    private lateinit var controller: GameLogicController
+    private lateinit var service: GameLogicService
+    private lateinit var principal: Principal
+    private val playerId = UUID.randomUUID()
+    private val lobbyId = UUID.randomUUID()
+
+    @BeforeEach
+    fun setup(){
+        service = mock()
+        controller= GameLogicController(service)
+        principal = mock {
+            on { name } doReturn playerId.toString()
+        }
+
+    }
+
+    @Test
+    fun passTest(){
+        val msg = PlayerMessage("Player","PASS",null,null,lobbyId)
+        controller.processAction(msg, principal)
+        verify(service).pass(lobbyId, playerId)
+    }
+
+    @Test
+    fun cheatTest(){
+        val msg = PlayerMessage("Player","CHEAT","payload",null,lobbyId)
+        controller.processAction(msg, principal)
+        verify(service).cheatDuplicate(lobbyId, playerId,"payload")
+    }
+
+    @Test
+    fun catComboTest(){
+        val networkCards = listOf(CardNetworkPacket("Feral", CardType.FERAL_CAT))
+        val msg = PlayerMessage("Player","CAT_COMBO",networkCards, null,lobbyId)
+
+        controller.processAction(msg, principal)
+
+        verify(service).playCatCombo(
+            eq(lobbyId),
+            eq(playerId),
+            argThat { this[0].type == CardType.FERAL_CAT },
+            isNull()
+        )
+    }
+
+    @Test
+    fun chooseFromDiscard(){
+        val card = CardNetworkPacket("Feral", CardType.FERAL_CAT)
+        val msg = PlayerMessage("Player","CHOOSE_FROM_DISCARD",listOf(card), null,lobbyId)
+
+        controller.processAction(msg, principal)
+        verify(service).chooseFromDiscard(lobbyId,playerId,CardType.FERAL_CAT)
+    }
+
+    @Test
+    fun chooseFromDiscardNullTest(){
+        val msg = PlayerMessage("Player","CHOOSE_FROM_DISCARD",emptyList<CardNetworkPacket>(),null,lobbyId)
+
+        controller.processAction(msg, principal)
+        verify(service).sendUserError(eq(playerId),any())
+    }
+
+    @Test
+    fun unknownActionTest(){
+        val msg = PlayerMessage("Player","UNKNOWN",null,null,lobbyId)
+
+        controller.processAction(msg, principal)
+        verify(service).sendUserError(eq(playerId),any())
+    }
 
     @LocalServerPort
     private var port: Int = 0
