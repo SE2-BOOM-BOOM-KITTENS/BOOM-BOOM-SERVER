0fc77e275b193982a6b13fb77919ee01f69b4567
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-14 16:41:03 +0200

    Set up GameLogicService tests

--- src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/services/GameLogicServiceTest.kt
+++ src/test/kotlin/com/aau/se2/boomboomkittens/filipp/server/services/GameLogicServiceTest.kt
@@ -1,15 +1,24 @@
 package com.aau.se2.boomboomkittens.filipp.server.services
 
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.networkPacket.messages.ServerMessage
 import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.filipp.server.services.GameLogicService
+import com.aau.se2.boomboomkittens.game.Lobby
 import com.aau.se2.boomboomkittens.game.cards.Card
 import com.aau.se2.boomboomkittens.game.cards.CardType
 import com.aau.se2.boomboomkittens.game.player.Player
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argThat
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import java.util.UUID
 
@@ -21,18 +30,74 @@ class GameLogicServiceTest {
 
     private lateinit var player: Player
     private lateinit var target: Player
-
-    //QUICK FIX; SHOULD BE REIMPLEMENTED LATER
-    private var lobbyId: UUID? = null
+    private lateinit var lobby: Lobby
 
     @BeforeEach
     fun setup() {
         service = GameLogicService(messagingTemplate,jacksonObjectMapper)
-        lobbyId = service.lobbyId
+
         player = Player(name = "Tester")
         target = Player(name = "Opfer")
-        service.joinGame(lobbyId!!,player.playerId, player.name)
-        service.joinGame(lobbyId!!,target.playerId, target.name)
+        lobby = Lobby(creator = player, maxPlayers = 4)
+        service.createGame(lobby)
+        service.joinGame(lobby.id,player.playerId, player.name)
+        service.joinGame(lobby.id,target.playerId, target.name)
+    }
+
+    @Test
+    fun getGameThrowTest(){
+
+        assertThrows(IllegalArgumentException::class.java){
+            service.getGame(UUID.randomUUID())
+        }
+    }
+
+    @Test
+    fun passTest(){
+    }
+
+    @Test
+    fun playCardsTest(){
+
+    }
+
+    @Test
+    fun cheatDuplicateTest(){
+
+    }
+
+    @Test
+    fun checkIfDuplicateTest(){
+    }
+
+    @Test
+    fun exitPlayerTest(){
+
+    }
+
+    @Test
+    fun getInitState(){
+
+    }
+
+    @Test
+    fun explodePlayerTest(){
+
+    }
+
+    @Test
+    fun endTurnTest(){
+
+    }
+
+    @Test
+    fun sendGameStateTest(){
+
+    }
+
+    @Test
+    fun sendErrorTest(){
+
     }
 
     @Test
@@ -44,7 +109,7 @@ class GameLogicServiceTest {
 
         val cards = listOf(Card(CardType.CAT_TACO), Card(CardType.CAT_TACO))
 
-        service.playCatCombo(lobbyId!!,player.playerId, cards, target.playerId)
+        service.playCatCombo(lobby.id,player.playerId, cards, target.playerId)
 
         //assertEquals(1, player.playerHand.getCardAmount())
         //assertEquals(0, target.playerHand.getCardAmount())
@@ -53,11 +118,11 @@ class GameLogicServiceTest {
     @Test
     fun `chooseFromDiscard should move card to player hand`() {
         val discardCard = Card(CardType.SHUFFLE)
-        service.getGame(lobbyId!!).discardPile.add(discardCard)
+        service.getGame(lobby.id).discardPile.add(discardCard)
 
-        service.chooseFromDiscard(lobbyId!!,player.playerId, CardType.SHUFFLE)
+        service.chooseFromDiscard(lobby.id,player.playerId, CardType.SHUFFLE)
 
-        val hand = service.getGame(lobbyId!!).getPlayerById(player.playerId)?.playerHand
+        val hand = service.getGame(lobby.id).getPlayerById(player.playerId)?.playerHand
         assertTrue(hand!!.containsCardType(CardType.SHUFFLE))
     }
 }
