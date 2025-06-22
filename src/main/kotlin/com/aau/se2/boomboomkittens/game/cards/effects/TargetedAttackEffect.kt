2a324770c9dc6f75c6b60959e78831699aa3e3a7
elsarny <elsarny@edu.aau.at>
2025-06-22 22:57:32 +0200

    TargetedAttackEffect-Klasse angepasst

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/TargetedAttackEffect.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/TargetedAttackEffect.kt
@@ -1,12 +1,13 @@
-package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects
+package com.aau.se2.boomboomkittens.game.cards.effects
 
+import com.aau.se2.boomboomkittens.game.cards.Card
 import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffect
-import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.GameLogic
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.CardLogic
 import com.aau.se2.boomboomkittens.game.player.Player
 
 class TargetedAttackEffect(private val target: Player) : CardEffect {
 
-    override fun apply(player: Player, gameLogic: GameLogic) {
-        gameLogic.forceNextPlayerToDrawExtraCards(target, 2)
+    override fun apply(card: Card, player: Player, cardLogic: CardLogic) {
+        cardLogic.gameLogic.forceNextPlayerToDrawExtraCards(target, 2)
     }
 }
