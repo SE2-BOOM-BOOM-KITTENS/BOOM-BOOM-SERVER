5eedceee181149f231df039224ec827ae835f5ca
elsarny <elsarny@edu.aau.at>
2025-06-22 23:37:45 +0200

    TargetedAttackEffect-Klasse angepasst

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/logic/GameLogic.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/logic/GameLogic.kt
@@ -55,7 +55,7 @@ open class GameLogic(
     fun getPlayerHand(playerId: UUID): PlayerHand? {
         return _cardLogic.getPlayerHand(playerId)
     }
-    fun forceNextPlayerToDrawExtraCards(player: Unit, amount: Int) {
+    fun forceNextPlayerToDrawExtraCards(player: Player, amount: Int) {
         if (amount <= 0) return
 
         repeat(amount) {
