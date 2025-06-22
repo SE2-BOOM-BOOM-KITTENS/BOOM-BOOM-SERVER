88285c716160683a8825cb452de91bf09a4ede9d
elsarny <elsarny@edu.aau.at>
2025-06-22 21:18:59 +0200

    TargetedAttackEffect geschrieben

--- src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/TargetedAttackEffect.kt
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/TargetedAttackEffect.kt
@@ -1,4 +1,10 @@
 package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffect
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.GameLogic
+import com.aau.se2.boomboomkittens.game.player.Player
 
-class TargetedAttackEffect {
+class TargetedAttackEffect(private val target: Player) : CardEffect {
+    override fun apply(player: Player, gameLogic: GameLogic) {
+        gameLogic.forceNextPlayerToDrawExtraCards(target, 2)
+    }
 }
