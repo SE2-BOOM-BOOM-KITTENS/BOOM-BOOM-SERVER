4af08e9c85127bb533ecfc9ebb984e55b2de6575
Marlene Simon <marsimon@edu.aau.at>
2025-06-23 11:53:34 +0200

    create attack effect

---
+++ src/main/kotlin/com/aau/se2/boomboomkittens/game/cards/effects/AttackEffect.kt
@@ -0,0 +1,18 @@
+package com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects
+
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.cards.effects.registry.CardEffect
+import com.aau.se2.boomboomkittens.com.aau.se2.boomboomkittens.game.logic.CardLogic
+import com.aau.se2.boomboomkittens.game.cards.Card
+import com.aau.se2.boomboomkittens.game.player.Player
+
+class AttackEffect : CardEffect {
+    override fun apply (card: Card, player: Player, cardLogic: CardLogic){
+        val gameLogic = cardLogic.gameLogic
+
+        gameLogic.nextTurn()
+        //gameLogic.giveExtraTurnToNextPlayer() TODO: implement method in gameLogic
+
+        println("${player.name} played an ATTACK card!")
+
+    }
+}
